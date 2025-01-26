0d121f6ca4c181a8d14465776e383610851ed1d5
TsiganovDS <118276245+TsiganovDS@users.noreply.github.com>
2025-01-26 17:42:22 +0700

    category.py

--- src/category.py
+++ src/category.py
@@ -1,18 +1,4 @@
-import json
-import os
-from typing import Any
-
-
-class Product:
-
-    def __init__(self, name: str, description: str, price: float, quantity: int) -> None:
-        self.name = name
-        self.description = description
-        self.price = price
-        self.quantity = quantity
-
-    def __repr__(self) -> str:
-        return f"{self.name!r}"
+from src.product import Product
 
 
 class Category:
@@ -22,6 +8,22 @@ class Category:
     def __init__(self, name: str, description: str, products: list[Product]) -> None:
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products if products is not None else []
         Category.category_count += 1
-        Category.product_count += len(products)
+        Category.product_count += len(self.__products)
+
+    @property
+    def products(self) -> list[Product]:
+        return self.__products
+
+    def add_product(self, new_product: Product) -> None:
+        if isinstance(new_product, Product):
+            self.__products.append(new_product)
+            Category.product_count += 1
+        else:
+            raise TypeError
+
+    @property
+    def product(self) -> list:
+        # Формируем список строк с информацией о каждом продукте
+        return [f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт." for product in self.__products]
