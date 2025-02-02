42cf786328ed3b1f460a0a14c9dd8ede0940ed07
TsiganovDS <118276245+TsiganovDS@users.noreply.github.com>
2025-02-02 22:08:26 +0700

    category.py

--- src/category.py
+++ src/category.py
@@ -25,11 +25,24 @@ class Category:
 
     @property
     def product(self) -> list:
-        # Формируем список строк с информацией о каждом продукте
+        """Формируем список строк с информацией о каждом продукте"""
         return [f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт." for product in self.__products]
 
     def __str__(self) -> str:
         counter = 0
         for product in self.__products:
             counter += product.quantity
+        return f"{self.name}, {counter} шт."
+
+    def middle_price(self) -> float:
+        """Подсчитывает среднюю цену всех товаров в категории."""
+        if not self.__products:
+            return 0.0
+
+        total_price = sum(product.price for product in self.__products)
+        average = round(total_price / len(self.__products), 2)
+        return average
+
+    def __repr__(self) -> str:
+        counter = sum(product.quantity for product in self.__products)
         return f"{self.name}, {counter} шт."
